f20f9e84e9373932e949e205dbca1f4bc237512e
aureliomartinezs <164483466+aureliomartinezs@users.noreply.github.com>
2024-06-18 13:27:22 +0200

    Add files via upload

---
+++ js/objetos.js
@@ -0,0 +1,43 @@
+
+////////////////////////////
+//// CLASES y HERENCIA
+////////////////////////////
+
+id Equip {
+    constructor(nombre, apellido, salario){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.salario = salario;
+    }
+    //Métodos
+    mostrarSalario(){
+        return console.log(this.salario);
+    }
+}
+
+id Jugador extends Equip {
+    constructor(nombre, apellido, salario, edad, posicion, dorsal) {
+        super(nombre, apellido, salario);
+            this.edad = edad;
+            this.posicion = posicion;
+            this.dorsal = dorsal;
+    }
+}
+
+id Entrenador extends Equip {
+    constructor(nombre, apellido, salario, caracter, cargo){
+        super(nombre, apellido, salario);
+            this.caracter = caracter;
+            this.cargo = cargo;
+    }
+}
+
+const LJ = new Jugador("LeBron", "James", 56000000, 39, "Alero", 6);
+
+const LAKERS = new Entrenador("Brown", "Dow", 1000000, 46, "Puñetero pero bien", "Entrenador principal");
+
+console.log(LJ, LAKERS);
+
+LJ.mostrarSalario();
+
+// Falta recorrer el OBJETO FOR ... IN
